8894e5b0191d9ff7b28c1073e8556a2847d7a885
jyoti140220 <71928435+jyoti140220@users.noreply.github.com>
2021-04-24 13:39:49 +0530

    Add files via upload

---
+++ demo3.js
@@ -0,0 +1,18 @@
+function multiplication()
+    {
+
+        var num1,num2,res;
+        num1=Number(document.formcalc.textnum1.value);
+        num2=Number(document.formcalc.textnum2.value);
+        document.getElementById("demo").innerHTML=num1*num2
+    }
+function divi()
+{
+
+    var num1,num2,res;
+    num1=Number(document.formcalc.textnum1.value);
+    num2=Number(document.formcalc.textnum2.value);
+    document.getElementById("demo").innerHTML=num1/num2
+
+    
+}
